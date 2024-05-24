0ad8f5826bc527b6f183701d1d84cac6e5d9b79d
Lucia Vu <luciavu6@outlook.com>
2024-05-25 04:19:24 +1000

    Add decimal, sign switch and percentage functionality

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ const MAX_LENGTH = 9;
 function appendToDisplay(char) {
     const display = document.querySelector(".result");
     if (display.innerHTML.length < MAX_LENGTH) {
+        // Only allow one decimal in each input
+        if (char == '.') {
+            if (display.innerHTML.includes('.')) {
+                return
+            };
+        };
         display.innerHTML += char;
         adjustFontSize();
-    }
-}
+    };
+};
 
 function adjustFontSize() {
     const display = document.querySelector(".result");
@@ -15,15 +21,37 @@ function adjustFontSize() {
     const maxLength = Math.round(fontSize/12);
 
     if (display.innerHTML.length >= maxLength) {
-        fontSize -= fontSize/10
-    }
+        fontSize -= fontSize/10;
+    };
     console.log(fontSize);
-    display.style.fontSize = fontSize + 'px'
-}
+    display.style.fontSize = fontSize + 'px';
+};
 
 function clearDisplay() {
     const display = document.querySelector(".result");
     display.innerHTML = "";
-    display.style.fontSize = '3.5rem'
+    display.style.fontSize = '3.5rem';
+};
+
+function switchSign() {
+    const display = document.querySelector(".result");
+    if (display.innerHTML.charAt(0) == '-') {
+        display.innerHTML = display.innerHTML.slice(1);
+    } else {
+        display.innerHTML = '-' + display.innerHTML;
+    };
+};
+
+function percentage() {
+    const display = document.querySelector(".result");
+    let integer = parseFloat(display.innerHTML);
+    let percentage = integer/100;
+    console.log(integer, percentage, String(percentage).length);
+    if (String(percentage).length <= MAX_LENGTH) {
+        display.innerHTML = percentage;
+    }
 }
 
+function calculate(operator) {
+    let operatorButton = document.querySelector(`.${operator}`);
+};
